e50d912930a49a0746516d9b41b442344832d908
Emmeline Vouriot <emmeline.vouriot@etu.univ-cotedazur.fr>
2020-12-07 17:34:57 +0100

    update test generator

--- tests/controller_cpp/shift_add_sub_mov.cpp
+++ tests/controller_cpp/shift_add_sub_mov.cpp
@@ -11,7 +11,7 @@ void Tester::generateLsl()
         m_outFile << "00000" << bitset<5>(imm5) << bitset<3>(rn) << bitset<3>(rd) << " ";
         m_outFile << "0000 0 0 ";
         m_outFile << "0 1 0 " << bitset<5>(imm5) << " xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx 0010 ";
-        m_outFile << "xxx " << bitset<3>(rn) << " " << bitset<3>(rd) << " xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx 0 0 xxxxxxxx" << endl;
+        m_outFile << "xxx " << bitset<3>(rn) << " " << bitset<3>(rd) << " xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx 0 0 xxxxxxxxxxxxxxxx" << endl;
     }
     m_outFile << endl;
 }
@@ -25,7 +25,7 @@ void Tester::generateLsr()
         m_outFile << "00001" << bitset<5>(imm5) << bitset<3>(rn) << bitset<3>(rd) << " ";
         m_outFile << "0000 0 0 ";
         m_outFile << "0 1 0 " << bitset<5>(imm5) << " xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx 0011 ";
-        m_outFile << "xxx " << bitset<3>(rn) << " " << bitset<3>(rd) << " xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx 0 0 xxxxxxxx" << endl;
+        m_outFile << "xxx " << bitset<3>(rn) << " " << bitset<3>(rd) << " xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx 0 0 xxxxxxxxxxxxxxxx" << endl;
     }
     m_outFile << endl;
 }
@@ -39,7 +39,7 @@ void Tester::generateAsr()
         m_outFile << "00010" << bitset<5>(imm5) << bitset<3>(rn) << bitset<3>(rd) << " ";
         m_outFile << "0000 0 0 ";
         m_outFile << "0 1 0 " << bitset<5>(imm5) << " xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx 0100 ";
-        m_outFile << "xxx " << bitset<3>(rn) << " " << bitset<3>(rd) << " xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx 0 0 xxxxxxxx" << endl;
+        m_outFile << "xxx " << bitset<3>(rn) << " " << bitset<3>(rd) << " xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx 0 0 xxxxxxxxxxxxxxxx" << endl;
     }
     m_outFile << endl;
 }
@@ -53,7 +53,7 @@ void Tester::generateAdd()
         m_outFile << "0001100" << bitset<3>(rm) << bitset<3>(rn) << bitset<3>(rd) << " ";
         m_outFile << "0000 0 0 ";
         m_outFile << "0 1 0 " << "xxxxx" << " xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx 0101 ";
-        m_outFile << bitset<3>(rm) << " " << bitset<3>(rn) << " " << bitset<3>(rd) << " xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx 0 0 xxxxxxxx" << endl;
+        m_outFile << bitset<3>(rm) << " " << bitset<3>(rn) << " " << bitset<3>(rd) << " xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx 0 0 xxxxxxxxxxxxxxxx" << endl;
     }
     m_outFile << endl;
 }
@@ -67,7 +67,7 @@ void Tester::generateSub()
         m_outFile << "0001101" << bitset<3>(rm) << bitset<3>(rn) << bitset<3>(rd) << " ";
         m_outFile << "0000 0 0 ";
         m_outFile << "1 1 0 " << "xxxxx" << " xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx 0110 ";
-        m_outFile << bitset<3>(rm) << " " << bitset<3>(rn) << " " << bitset<3>(rd) << " xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx 0 0 xxxxxxxx" << endl;
+        m_outFile << bitset<3>(rm) << " " << bitset<3>(rn) << " " << bitset<3>(rd) << " xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx 0 0 xxxxxxxxxxxxxxxx" << endl;
     }
     m_outFile << endl;
 }
@@ -81,7 +81,7 @@ void Tester::generateAddImm()
         m_outFile << "0001110" << bitset<3>(imm3) << bitset<3>(rn) << bitset<3>(rd) << " ";
         m_outFile << "0000 0 0 ";
         m_outFile << "0 1 1 " << "10" << bitset<3>(imm3) << " xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx 0101 ";
-        m_outFile << "xxx " << bitset<3>(rn) << " " << bitset<3>(rd) << " xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx 0 0 xxxxxxxx" << endl;
+        m_outFile << "xxx " << bitset<3>(rn) << " " << bitset<3>(rd) << " xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx 0 0 xxxxxxxxxxxxxxxx" << endl;
     }
     m_outFile << endl;
 }
@@ -95,7 +95,7 @@ void Tester::generateSubImm()
         m_outFile << "0001111" << bitset<3>(imm3) << bitset<3>(rn) << bitset<3>(rd) << " ";
         m_outFile << "0000 0 0 ";
         m_outFile << "1 1 1 " << "11" << bitset<3>(imm3) << " xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx 0110 ";
-        m_outFile << "xxx " << bitset<3>(rn) << " " << bitset<3>(rd) << " xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx 0 0 xxxxxxxx" << endl;
+        m_outFile << "xxx " << bitset<3>(rn) << " " << bitset<3>(rd) << " xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx 0 0 xxxxxxxxxxxxxxxx" << endl;
     }
     m_outFile << endl;
 }
@@ -109,7 +109,7 @@ void Tester::generateMovImm()
         m_outFile << "00100" << bitset<3>(rd) << bitset<8>(imm8) << " ";
         m_outFile << "0000 0 0 ";
         m_outFile << "0 1 1 " << "xxxxx" << " xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx xxxx ";
-        m_outFile << "xxx " << "xxx" << " " << bitset<3>(rd) << " xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx 0 0 xxxxxxxx" << endl;
+        m_outFile << "xxx " << "xxx" << " " << bitset<3>(rd) << " xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx 0 0 xxxxxxxxxxxxxxxx" << endl;
     }
     m_outFile << endl;
 }
