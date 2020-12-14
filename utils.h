f61fc1d643935b5e6b7446a8d0a40106afd2e9a1
Thomas Prévost <thomas.prevost@etu.univ-cotedazur.fr>
2020-12-14 17:18:38 +0100

    Update utils.h

--- utils.h
+++ utils.h
@@ -4,8 +4,8 @@
 #include "pins.h"
 
 #define abs(x) (x<0 ? (-x) : x)
-#define div(a, b, c) for(int i=-120; i<=abs(a); i++){if(i*abs(b)>=abs(a)){c=(a*b<0) ? -i : i;break;}} // c=a/b
-#define divPos(a, b, c) for(int i=0; i<=a; i++){if(i*b>=a){c=i;break;}}
-#define moduloPos(a, b, c) int x;divPos(a, b, x);c=abs((x*b)-a);
+#define div(a, b, c) for(int iDD=-120; iDD<=abs(a); iDD++){if(iDD*abs(b)>=abs(a)){c=(a*b<0) ? -iDD : iDD;break;}} // c=a/b
+#define divPos(a, b, c) for(int iGG=0; iGG<=a; iGG++){if(iGG*b>=a){c=iGG;break;}}
+#define moduloPos(a, b, c) int xJJ;divPos(a, b, xJJ);c=abs((xJJ*b)-a);
 
 #endif // UTILS_H_INCLUDED
