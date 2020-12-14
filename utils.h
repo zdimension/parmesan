40a3aa3f6f6b7e49fee069db97869abe17dc49c1
Thomas Prévost <thomas.prevost@etu.univ-cotedazur.fr>
2020-12-14 16:53:54 +0100

    Create utils.h

---
+++ utils.h
@@ -0,0 +1,10 @@
+#ifndef UTILS_H_INCLUDED
+#define UTILS_H_INCLUDED
+
+#include "pins.h"
+
+#define abs(x) (x<0 ? (-x) : x)
+#define div(a, b, c) for(int i=-120; i<=abs(a); i++){if(i*abs(b)>=abs(a)){c=(a*b<0) ? -i : i;break;}} // c=a/b
+#define divPos(a, b, c) for(int i=0; i<=a; i++){if(i*b>=a){c=i;break;}}
+
+#endif // UTILS_H_INCLUDED
