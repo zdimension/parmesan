4b2b3834525133411c02715126923499b8803453
Thomas Prévost <thomas.prevost@etu.univ-cotedazur.fr>
2020-12-14 17:23:24 +0100

    Update utils.h

--- utils.h
+++ utils.h
@@ -6,6 +6,6 @@
 #define abs(x) (x<0 ? (-x) : x)
 #define div(a, b, c) for(int iDD=-120; iDD<=abs(a); iDD++){if(iDD*abs(b)>=abs(a)){c=(a*b<0) ? -iDD : iDD;break;}} // c=a/b
 #define divPos(a, b, c) for(int iGG=0; iGG<=a; iGG++){if(iGG*b>=a){c=iGG;break;}}
-#define moduloPos(a, b, c) int xJJ;divPos(a, b, xJJ);c=abs((xJJ*b)-a);
+#define moduloPos(a, b, c) {int xJJ;divPos(a, b, xJJ);c=abs((xJJ*b)-a);}
 
 #endif // UTILS_H_INCLUDED
