b281252a2df4bb024be0d9ad889a1d391c5ae69e
Emmeline Vouriot <emmeline.vouriot@etu.univ-cotedazur.fr>
2020-12-30 16:41:22 +0100

    ajout en commentaire d'un chemin

--- tests/controller_cpp/main.cpp
+++ tests/controller_cpp/main.cpp
@@ -5,6 +5,7 @@ using namespace std;
 
 int main()
 {
+    //Tester tester(R"(C:\Users\Emmy\Documents\BOSSAGE\BAC +3\S5\archi\parmesan\parmesan\tests", 50);
     Tester tester("C:\\Users\\thoma\\Documents\\travail\\polytech\\SI3\\archi\\tests\\test_controller.txt", 50);
     tester.generateTest(Tester::LDR);
     return 0;
