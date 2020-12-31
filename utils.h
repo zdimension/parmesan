bb912fc1bf4c99b80e1169092ed3afbd5e982d39
Tom Niget <tom.niget@etu.univ-cotedazur.fr>
2020-12-31 14:18:43 +0100

    fixed-point numbers macros

--- utils.h
+++ utils.h
@@ -87,4 +87,48 @@
 		x *= 10;\
 		x += cur - '0' + 1;\
 	}\
-}
+	x;\
+})
+
+#define TOFP(val) ((unsigned int)((val) * (1 << 16))) 
+#define MULTFP(x, y) (((x) >> 8) * ((y) >> 8))
+#define DIVFP(x, y) DIV((x) << 8, y)
+
+#define PRINTFP(x) do {\
+	unsigned int num = x;\
+	unsigned int tmp;\
+	asm(\
+		"movs r0, %[val]		\n"\
+		"lsrs %[res], r0, 16	  " : [res] "=&r" (tmp) : [val] "r" (num) : "r0");\
+	RES = tmp;\
+	PRINTRES_FIX(5);\
+	asm(\
+		"lsls r2, %[val], #16	\n"\
+		"lsrs r2, r2, #16		\n"\
+		"movs r1, #100			\n"\
+		"muls r2, r2, r1		\n"\
+		"muls r2, r2, r1		\n"\
+		"lsrs %[res], r2, #16	  " : [res] "=&r" (tmp) : [val] "r" (num) : "r2", "r1");\
+	RES = tmp;\
+	PUTCHAR('.');\
+	PRINTRES_FIX(4);\
+} while(0)
+
+#define SQRTFP(x) ({\
+	unsigned int t, q, b, r;\
+    r = x;\
+    b = 0x40000000;\
+    q = 0;\
+    while( b > 0x40 )\
+    {\
+        t = q + b;\
+        if( r >= t )\
+        {\
+            r -= t;\
+            q = t + b;\
+        }\
+        r <<= 1;\
+        b >>= 1;\
+    }\
+    q >> 8;\
+})
