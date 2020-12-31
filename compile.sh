0b12eff87f93393a6495de3ef3353c62b76f40d2
Tom Niget <tom.niget@etu.univ-cotedazur.fr>
2020-12-31 13:34:59 +0100

    restrict clang to ARMv4t

--- compile.sh
+++ compile.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 filename="$1"
-clang-8 -S -target arm-none-eabi -nostdlib -mcpu=cortex-m0 -mthumb -O0 -mfloat-abi=soft ${filename} && ./assembleur_tom.py ${filename%.*}.s
+clang-8 -S -target arm-v4t-eabi -nostdlib -mcpu=cortex-m0 -mthumb -O0 -mfloat-abi=soft ${filename} && ./assembleur_tom.py ${filename%.*}.s
