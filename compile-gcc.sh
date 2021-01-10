f8830808cc81670cf491684d934fbae0d92b1d0a
Tom Niget <tom.niget@etu.univ-cotedazur.fr>
2021-01-10 11:54:25 +0100

    Fix compilation scripts

--- compile-gcc.sh
+++ compile-gcc.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 filename="$1"
-arm-none-eabi-gcc -S -mthumb -nostdlib -mcpu=cortex-m0 -fomit-frame-pointer -O0 ${filename} && ./assembleur_tom.py ${filename%.*}.s
+arm-none-eabi-gcc -S -mthumb -nostdlib -mtune=cortex-m0 -mcpu=cortex-m0 -fomit-frame-pointer -O0 ${filename} && ./assembleur_tom.py ${filename%.*}.s
